11658b73bd294f9dcdd47ea9abac4c4e7fa20178
erroldepa <errold.epa@gmail.com>
2025-10-20 01:33:35 +0800

    revert medusa-config

--- backend/test-env.js
+++ backend/test-env.js
@@ -1,7 +1,7 @@
 console.log("--- Starting Environment Variable Test ---");
 
-const dbUrl = process.env.DATABASE_URL;
-const redisUrl = process.env.REDIS_URL;
+const dbUrl = process.env.DATABASE_URL || "postgresql://tons_hub_db_user:m8GRDlxjw30LParrOX1qZTYF4RQX2LSP@dpg-d3pks063jp1c738213ng-a/tons_hub_db";
+const redisUrl = process.env.REDIS_URL || "redis://default:AUppAAIncDIyOTBmYzY0MWU4YTg0ZmM1YmUxZGQ2MWU1MmI2MDNmOXAyMTkwNDk@comic-bream-19049.upstash.io:6379";
 
 console.log(`DATABASE_URL is: ${dbUrl}`);
 console.log(`REDIS_URL is: ${redisUrl}`);
